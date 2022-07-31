bee14dc3685584caf1dfd65bc27942073c63adab
nuthan-fixity <109649629+nuthan-fixity@users.noreply.github.com>
2022-07-31 12:46:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Idly')
-streamlit.text('Dosa')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
